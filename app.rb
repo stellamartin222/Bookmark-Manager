5490dc68792167cba8f6759fc982b79339afe0a9
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-13 16:46:45 +0100

    feat(*): fix update routing

--- app.rb
+++ app.rb
@@ -29,13 +29,11 @@ class BookmarkManager < Sinatra::Base
 
     get '/bookmarks/update/:id' do
         @bookmark = Bookmark.find(params[:id])
-        session[:id] = @bookmark.id
         erb(:update_bookmark)
     end
 
-    post '/bookmarks/update/:title/:url' do
-        @bookmark = Bookmark.find(session[:id])
-        @bookmark.update(params[:url], params[:title])
+    post '/:id/bookmarks/update' do
+        Bookmark.find(params[:id]).update(params[:url], params[:title])
         redirect("/bookmarks")
     end
 end
